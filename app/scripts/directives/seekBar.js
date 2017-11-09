7d4dfb83bd7839e4351acf5be3e116ed5116476a
Wan Cheul Kim <wckim88@gmail.com>
2017-11-08 23:54:42 -0800

    Checkpoint 10: Seek-bar updates relate to current song playback time

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -19,7 +19,9 @@
       templateUrl: '/templates/directives/seek_bar.html',
       replace: true,
       restrict: 'E',
-      scope: { },
+      scope: {
+        onChange: '&'
+      },
       link: function(scope, element, attribute) {
         scope.value = 0;
         scope.max = 100;
@@ -29,6 +31,14 @@
         * @type element?
         */
         var seekBar = $(element);
+
+        attribute.$observe('value', function(newValue) {
+          scope.value = newValue;
+        });
+
+        attribute.$observe('max', function(newValue) {
+          scope.max = newValue;
+        });
 
         /**
         * @function percentString
@@ -59,6 +69,7 @@
         scope.onClickSeekBar = function(event) {
           var percent = calculatePercent(seekBar, event);
           scope.value = percent * scope.max;
+          notifyOnChange(scope.value);
         };
 
         /**
@@ -70,6 +81,7 @@
             var percent = calculatePercent(seekBar, event);
             scope.$apply(function() {
               scope.value = percent * scope.max;
+              notifyOnChange(scope.value);
             });
           });
 
@@ -86,6 +98,17 @@
         */
         scope.thumbStyle = function() {
           return {left: percentString()};
+        };
+
+        /**
+        * @function notifyOnChange
+        * @desc
+        * @param {}
+        */
+        var notifyOnChange = function(newValue) {
+          if (typeof scope.onChange === 'function') {
+            scope.onChange({value: newValue});
+          }
         };
       }
     };
--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-  function SongPlayer(Fixtures) {
+  function SongPlayer($rootScope, Fixtures) {
     var SongPlayer = {};
 
     var currentAlbum = Fixtures.getAlbum();
@@ -36,6 +36,12 @@
           preload: true
       });
 
+      currentBuzzObject.bind('timeupdate', function() {
+        $rootScope.$apply(function() {
+          SongPlayer.currentTime = currentBuzzObject.getTime();
+        });
+      });
+
       SongPlayer.currentSong = song;
     };
 
@@ -64,6 +70,12 @@
     * @type {Object}
     */
     setSong(currentAlbum.songs[0]);
+
+    /**
+    * @desc Current playback time(in seconds) of currently playing song
+    * @type {Number}
+    */
+    SongPlayer.currentTime = null;
 
     /**
     * @function play
@@ -127,10 +139,21 @@
       }
     };
 
+    /**
+    * @function setCurrentTime
+    * @desc Set current time(in seconds) of currently playing song
+    * @param {Number} time
+    */
+    SongPlayer.setCurrentTime = function(time) {
+      if (currentBuzzObject) {
+        currentBuzzObject.setTime(time);
+      }
+    };
+
     return SongPlayer;
   }
 
   angular
     .module('blocJams')
-    .factory('SongPlayer', ['Fixtures', SongPlayer])
+    .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer])
 })();
