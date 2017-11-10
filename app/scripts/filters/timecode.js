a9e62556cdc6cabb6c1e3cdd1470764161a153c4
Wan Cheul Kim <wckim88@gmail.com>
2017-11-09 19:41:32 -0800

    Assignment 11: Optional - timecode modified to use Buzz method

--- app/scripts/filters/timecode.js
+++ app/scripts/filters/timecode.js
@@ -7,17 +7,7 @@
         return '-:--';
       }
 
-      var wholeSeconds = Math.floor(seconds);
-      var minutes = Math.floor(wholeSeconds / 60);
-      var remainingSeconds = wholeSeconds % 60;
-
-      var output = minutes + ':';
-
-      if (remainingSeconds < 10) {
-        output += '0';
-      }
-
-      output += remainingSeconds;
+      output = buzz.toTimer(seconds);
 
       return output;
     };
